a416293ffbb4fe8db28f3c46971dadd0ce4e5b47
GabrielGP1 <g.gascon-parent@hotmail.com>
2025-11-04 11:59:43 -0500

    test

--- myscript.py
+++ myscript.py
@@ -5,6 +5,5 @@ import os
 badhash = os.environ.get("BADHASH", "HEAD")
 goodhash = "e4cfc6f77ebbe2e23550ddab682316ab4ce1c03c"
 
-os.system("git bisect start $badhash $goodhash")
+os.system(f"git bisect start {badhash} {goodhash}")
 os.system("git bisect run python manage.py test")
-os.system("git bisect reset")
